5511da2c8cb9b2545c132ecfa59efa18ce57b3ff
Nidhal Jelassi <nidhal.jelassi@fsegt.utm.tn>
2024-08-28 14:50:54 +0100

    add ex6

--- ex5.js
+++ ex5.js
@@ -1,9 +1,12 @@
 let x = prompt("Veuillez saisir un entier");
 
 function test(nb) {
-  console.log(typeof nb);
-  if (nb < 10) alert("Votre valeur est inférieur à 10");
-  else alert("Votre valeur est supérieur à 10");
+  console.log(nb);
+  if (isNaN(nb)) console.log("erreur");
+  else {
+    if (nb < 10) alert("Votre valeur est inférieur à 10");
+    else alert("Votre valeur est supérieur à 10");
+  }
 }
 
 // test(parseInt(x));
