bafbc52b97eba84c73053025bf6d8ed9d1328445
Nidhal Jelassi <nidhal.jelassi@fsegt.utm.tn>
2024-08-28 12:35:18 +0100

    add ex3

--- ex2.js
+++ ex2.js
@@ -2,10 +2,27 @@ const input1 = document.getElementById("idPrenom");
 const input2 = document.getElementById("idNom");
 const btn = document.getElementById("afficherBtn");
 
-function traitement() {
-  console.log(input1.value, input2.value);
+// function traitement() {
+//   console.log(input1.value, input2.value);
+//   let msg = `Je m'appelle ${input1.value} ${input2.value}. Merci !`;
+//   alert(msg);
+// }
+
+btn.addEventListener("click", () => {
   let msg = `Je m'appelle ${input1.value} ${input2.value}. Merci !`;
   alert(msg);
-}
+});
 
-btn.addEventListener("click", traitement);
+// function addition(a, b) {
+//     return a + b;
+// }
+
+//  (a, b) => {
+//     return a + b;
+// }
+
+//  (a, b) =>  a + b;
+
+//  a =>  a + 10;
+
+//  () =>  20 + 10;
---
+++ ex3.js
@@ -0,0 +1,68 @@
+let tab = [
+  125,
+  "ABC",
+  214,
+  87,
+  "ERT",
+  "TYUIO",
+  254,
+  963,
+  47,
+  "Scrt",
+  "Sezat",
+  52,
+  47,
+  "Nidhal",
+  89,
+];
+
+let tabNumbers = tab.filter((element) => typeof element == "number");
+let tabStrings = tab.filter((element) => typeof element == "string");
+
+console.log(tabNumbers);
+console.log(tabStrings);
+
+tabStrings.sort();
+console.log(tabStrings);
+tabNumbers.sort((a, b) => a - b);
+/*
+si a -b < 0 donc a < b donc a sera placé avant b
+si a -b > 0 donc a > b donc a sera placé aprés b
+si a -b = 0 donc a = b donc l'ordre reste le même
+*/
+console.log(tabNumbers);
+
+tabStrings.reverse();
+console.log(tabStrings);
+
+let somme = tabNumbers.reduce((a, b) => a + b);
+let produit = tabNumbers.reduce((a, b) => a * b);
+let moyenne = somme / tabNumbers.length;
+console.log(somme, produit, moyenne);
+
+let max = Math.max(...tabNumbers);
+let min = Math.min(...tabNumbers);
+console.log(max, min);
+
+let randomNumber = 2;
+let elem = tabNumbers.includes(randomNumber);
+if (elem) {
+  console.log(
+    randomNumber,
+    "se trouve à la position ",
+    tabNumbers.indexOf(randomNumber)
+  );
+} else {
+  console.log("Element introuvable");
+}
+
+let tabSup = tabNumbers.filter((element) => element >= moyenne);
+let tabInf = tabNumbers.filter((element) => element < moyenne);
+console.log(tabSup, tabInf);
+
+let t = tabInf.concat(tabStrings);
+t = t.concat(tabSup);
+console.log(t);
+
+let t2 = [...tabInf, ...tabStrings, ...tabSup];
+console.log(t2);
